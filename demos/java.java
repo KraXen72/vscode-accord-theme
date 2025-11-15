e6a817deff02a3924849a591af827312d8857532
KraXen72 <dpelech1@gmail.com>
2025-11-15 16:42:06 +0100

    add more complex type tests for java

--- demos/java.java
+++ demos/java.java
@@ -71,3 +71,57 @@ class Solution {
         
     }
 }
+
+class Solution2 {
+
+    /**
+     * @param n the number of nodes
+     * @param m the number of edges
+     * @param edges the set of edges, with endpoints labelled between 1 and n inclusive.
+     * @return true iff there is a cycle in the graph
+     */
+    public static boolean isThereACycle2(int n, int m, Set<Edge> edges) {
+        var adjlist = new HashMap<Integer, ArrayList<Integer>>();
+        var inDegree = new int[n + 1];
+        
+        // Build adjacency list and in-degree array
+        for (Edge e : edges) {
+            adjlist.computeIfAbsent(e.from, k -> new ArrayList<>()).add(e.to);
+            inDegree[e.to]++;
+        }
+        
+        var queue = new ArrayDeque<Integer>();
+        // Enqueue all nodes with in-degree 0
+        for (int i = 1; i <= n; i++) {
+            if (inDegree[i] == 0) {
+                queue.add(i);
+            }
+        }
+        
+        int count = 0;
+        while (!queue.isEmpty()) {
+            int node = queue.removeFirst();
+            count++;
+            for (int neighbor : adjlist.getOrDefault(node, new ArrayList<>())) {
+                inDegree[neighbor]--;
+                if (inDegree[neighbor] == 0) {
+                    queue.add(neighbor);
+                }
+            }
+        }
+        
+        // If count != n, there is a cycle
+        return count != n;
+    } 
+
+	public static boolean isThereACycle3(
+		int n,
+		Set<Edge> edges
+		ArrayList<Integer> al,
+		Map<Integer, ArrayList<Integer>> mapper,
+		boolean monkey,
+		String yeah
+	) {
+		System.out.println("sigma.rizz")
+	}
+}
